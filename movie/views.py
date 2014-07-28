d0d129c8de97cb844e8ce1fcec36c42b28303213
Taehoon Kim <carpedm20@gmail.com>
2014-07-28 11:08:42 +0900

    practice d3.js

--- movie/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    template = "index.html"
-
-    return render(template)
--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,18 @@
-$( document ).ready(function() {
-  /* for front-end */
-  $(".reply-btn").click(function() {
-    if ($('#'+$(this).attr('name')).children().size() == 0) {
-      var id = $(this).attr('name');
-      var input = "<input type=\"hidden\" name=\"parent\" value=\"" + id + "\">";
-      var form = $("<div>").append($(write_form).append(input).clone()).html();
+(function() {
+  var w = 650;
+  var h = 480;
 
-      $('#'+$(this).attr('name')).append(form);
-    }
+  var data = d3.json("/static/data/data.json", function(error, root) {
+    var bodySelection = d3.select("body");
+
+    var svgSelection = bodySelection.append("svg")
+          .attr("width", w)
+          .attr("height", h);
+    var circleSelection = svgSelection.append("circle")
+          .attr("cx", w/2)
+          .attr("cy", h/2)
+          .attr("r", 50)
+          .style("fill", "purple");
+
   });
-
-  /* for backend */
-  $(this).find(".guide").twipsy({delayIn:1500,offset:1});
-
-});
+})();
