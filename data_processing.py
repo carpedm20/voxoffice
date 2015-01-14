bfcf5b007e9a729d360b776c29c1dd2ddcb1f83b
carpedm20 <carpedm20@gmail.com>
2015-01-14 23:04:27 +0900

    version 1 complete

--- data_processing.py
+++ data_processing.py
@@ -3,6 +3,7 @@ import numpy as np
 from collections import defaultdict
 
 MAX_RANK = 10
+SKIP_DATE = 4
 
 j = json.loads(open('static/movie0.json').read())
 
@@ -20,14 +21,16 @@ for year in years:
     end_year  = '%s1231' % year
 
     for i in j:
-        if start_year < i['date'] < end_year:
-            if i['rank'] > MAX_RANK:
-                continue
-            url = i['url']
-            code = int(url[url.index('code=')+5:])
-            movies[code] += 1
-            movie_title[code] = i['name']
-            dates[i['date']] += 1
+        dd = int(i['date'][-2:])
+        if dd % SKIP_DATE == 0 or dd in [28,29,30,31]:
+            if start_year < i['date'] < end_year:
+                if i['rank'] > MAX_RANK:
+                    continue
+                url = i['url']
+                code = int(url[url.index('code=')+5:])
+                movies[code] += 1
+                movie_title[code] = i['name']
+                dates[i['date']] += 1
 
     movies_key = movies.keys()
     movies_key.sort()
@@ -45,20 +48,23 @@ for year in years:
     movies_key = [i[0] for i in sorted(movies.iteritems(), key=lambda (k,v): v,reverse=True)]
 
     for i in j:
-        if start_year < i['date'] < end_year:
-            url = i['url']
-            code = int(url[url.index('code=')+5:])
-            try:
-                new_code = movies_key.index(code)
-            except:
-                continue
-            date = dates.index(i['date'])
-            new_movie_title[new_code] = movie_title[code]
-            y1[new_code][date] = int(i['rank'])
+        dd = int(i['date'][-2:])
+        if dd % SKIP_DATE == 0 or dd in [28,29,30,31]:
+            if start_year < i['date'] < end_year:
+                url = i['url']
+                code = int(url[url.index('code=')+5:])
+                try:
+                    new_code = movies_key.index(code)
+                except:
+                    continue
+                date = dates.index(i['date'])
+                new_movie_title[new_code] = movie_title[code]
+                y1[new_code][date] = int(i['rank'])
 
     ans = {'movies' : new_movie_title,
         'mindate' : dates[0],
         'maxdate' : dates[-1],
+        'skipdate' : SKIP_DATE,
         'y1' : y1.tolist() }
 
     with open('fox-%s.json' % year,'w') as f:
--- static/script.js
+++ static/script.js
@@ -5,49 +5,43 @@ $(document).ready(function() {
     $(".dropdown-button").dropdown();
 });
 
-var vertical = false;
-
 var format = d3.time.format("%Y%m%d");
+var format2 = d3.time.format("%b");
 
 var charts = [];
 
 var Chart = function(year) {
-    this.year = year;
+    var year = year;
 
     var margin = {top: 10, right: 10, bottom: 100, left: 40},
-        margin2 = {top: 530, right: 10, bottom: 20, left: 40},
-        width = $(".foxoffice").parent().width() - margin.left - margin.right - 30,
-        height = 600 - margin.top - margin.bottom,
-        height2 = 600 - margin2.top - margin2.bottom;
+        margin2 = {top: 430, right: 10, bottom: 20, left: 40},
+        width = $(".foxoffice").parent().width() - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom,
+        height2 = 500 - margin2.top - margin2.bottom;
 
-    this.svg = d3.select("#"+this.year).append("svg")
+    var svg = d3.select("#"+year).append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom);
 
-    if (vertical) {
-        var width = height, height = width,
-            x = d3.scale.linear().range([0, height]),
-            y = d3.scale.linear().range([width, 0]);
-    } else {
-        var x = d3.scale.linear().range([width, 0]),
-            x2 = d3.scale.linear().range([width, 0]),
-            y = d3.scale.linear().range([0, height]),
-            y2 = d3.scale.linear().range([0, 1]);
-    }
+    var x = d3.scale.linear().range([0, width]),
+        x2 = d3.scale.linear().range([0, width]),
+        y = d3.scale.linear().range([height, 0]),
+        y2 = d3.scale.linear().range([height2, 0]);
 
-    var xAxis = d3.svg.axis().scale(this.x).orient("bottom"),
-        xAxis2 = d3.svg.axis().scale(this.x2).orient("bottom"),
-        yAxis = d3.svg.axis().scale(this.y).orient("left");
+    var xAxis = d3.svg.axis().scale(x).orient("bottom")
+        .ticks(3, function(d, i) {}),
+        xAxis2 = d3.svg.axis().scale(x2).orient("bottom")
+        .ticks(12, function(d, i) {});
 
-    this.brushed = function() {
-        this.x.domain(this.brush.empty() ? this.x2.domain() : this.brush.extent());
-        focus.selectAll("path").attr("d", function(d) { return area(d.values); });
-        focus.select(".x.axis").call(this.xAxis);
-    }
+    var brush = d3.svg.brush()
+        .x(x2)
+        .on("brush", brushed);
 
-    this.brush = d3.svg.brush()
-        .x(this.x2)
-        .on("brush", this.brushed);
+    function brushed() {
+        x.domain(brush.empty() ? x2.domain() : brush.extent());
+        focus.select(".x.axis").call(xAxis);
+        focus.selectAll(".layer").attr("d", function(d) { return area(d.values); });
+    };
 
     var color = d3.scale.linear()
         .range(["#045A8D", "#F1EEF6"]);
@@ -62,42 +56,77 @@ var Chart = function(year) {
         .x(function(d) { return d.x; })
         .y(function(d) { return d.y; });
 
-    if (vertical)
-        var area = d3.svg.area()
-            //.interpolate("basis")
-            .y(function(d) { return this.x(d.x); })
-            .x0(function(d) { return this.y(d.y0); })
-            .x1(function(d) { return this.y(d.y0 + d.y); });
-    else
-        var area = d3.svg.area()
-            //.interpolate("basis")
-            .x(function(d) { return x(d.x); })
-            .y0(function(d) { return y(d.y0); })
-            .y1(function(d) { return y(d.y0 + d.y); });
-
-    this.area2 = d3.svg.area()
-        .x(function(d) { return x2(d.x); })
+    var area = d3.svg.area()
+        //.interpolate("basis")
+        .x(function(d) { return x(d.x); })
         .y0(function(d) { return y(d.y0); })
         .y1(function(d) { return y(d.y0 + d.y); });
 
-    var focus = this.svg.append("g")
+    var area2 = d3.svg.area()
+        .x(function(d) { return x2(d.x); })
+        .y0(height2)
+        .y1(function(d) { return y2(d.y); });
+
+    var focus = svg.append("g")
         .attr("class", "focus")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var context = this.svg.append("g")
+    var context = svg.append("g")
         .attr("class", "context")
         .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
     this.get_json = function() {
-        d3.json("./static/"+this.year+".json", this.process);
+        d3.json("./static/"+year+".json", this.process);
     };
 
     var layers = [];
 
+    var update_context = function(idx) {
+        path = context.select("path");
+
+        y2.domain([0, d3.max(layers[context_idx].values, function(value) {
+            return value.y;
+        })]);
+
+        if (path[0][0] == null)
+            path = context.append("path");
+        
+        path.datum(layers[idx])
+            .attr("class", "area")
+            .attr("d", function(d) { return area2(d.values); })
+            .style("fill", function(d) { return context_color; });
+
+        context.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height2 + ")")
+            .call(xAxis2);
+
+        focus.selectAll("path")
+            .style("fill", function(d) {
+                if (d.idx == idx)
+                    return context_color;
+                else
+                    return color(d.idx);
+            });
+    };
+
     this.process = function(error, data) {
         mindate = format.parse(data['mindate']);
         maxdate = format.parse(data['maxdate']);
+        skipdate = Number(data['skipdate']);
         y1 = data['y1'];
+
+        xAxis.tickFormat(function(d) {
+            date = new Date(mindate);
+            date.setDate(mindate.getDate() + d*2);
+            return format2(date);
+        });
+
+        xAxis2.tickFormat(function(d) {
+            date = new Date(mindate);
+            date.setDate(mindate.getDate() + d*2);
+            return format2(date);
+        });
 
         for (var idx in y1) {
             for (var jdx in y1[idx]) {
@@ -107,7 +136,7 @@ var Chart = function(year) {
                 }
                 tmp = y1[idx][jdx];
                 tmp = 1.0/tmp == Infinity ? 0 : 1.0/tmp;
-                layers[idx].values.push({//key:data['movies'][idx],
+                layers[idx].values.push({key:data['movies'][idx],
                                         x: Number(jdx),
                                         y: tmp});
             }
@@ -116,18 +145,15 @@ var Chart = function(year) {
         layers = stack(layers);
 
         var n = layers.length,
-            m = ((maxdate-mindate)/(1000*60*60*24));
+            m = ((maxdate-mindate)/(1000*60*60*24)/skipdate);
 
         x.domain([0, m - 1]);
         x2.domain([0, m - 1]);
         y.domain([0,5.5]);
 
-        y2.domain([0, d3.max(layers[0].values, function(value) {
-            return value.y;
-        })]);
         color.domain([0, layers.length]);
 
-        var path = focus.selectAll(".layer")
+        focus.selectAll(".layer")
             .data(layers)
             .enter().append("path")
             .attr("class", "layer")
@@ -146,11 +172,11 @@ var Chart = function(year) {
             .attr("transform", "translate(0," + height + ")")
             .call(xAxis);
 
-        this.update_context(0);
+        update_context(0);
 
         context.append("g")
             .attr("class", "x brush")
-            .call(this.brush)
+            .call(brush)
         .selectAll("rect")
             .attr("y", -6)
             .attr("height", height2 + 7);
@@ -165,7 +191,7 @@ var Chart = function(year) {
                     })
             })
             .on("click", function(d, i) {
-                this.update_context(d.idx);
+                update_context(d.idx);
             })
             .on("mouseout", function(d, i) {
                 svg.selectAll(".layer")
@@ -175,31 +201,6 @@ var Chart = function(year) {
                 d3.select(this)
                     .classed("hover", false)
                     .attr("stroke-width", "0px");
-            });
-    };
-
-    this.update_context = function(idx) {
-        path = this.context.select("path");
-
-        if (path[0][0] == null)
-            path = this.context.append("path");
-        
-        path.datum(layers[idx])
-            .attr("class", "area")
-            .attr("d", function(d) { return this.area2(d.values); })
-            .style("fill", function(d) { return this.context_color; });
-
-        this.context.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height2 + ")")
-            .call(this.xAxis2);
-
-        focus.selectAll("path")
-            .style("fill", function(d) {
-                if (d.idx == idx)
-                    return this.context_color;
-                else
-                    return this.color(d.idx);
             });
     };
 };
